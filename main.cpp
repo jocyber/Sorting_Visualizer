062555a1c592af3cd4525919a0a91b89a72d08bc
jocyber <jordaneharman@protonmail.com>
2021-03-22 17:31:43 -0400

    added selection sort

--- main.cpp
+++ main.cpp
@@ -8,9 +8,11 @@
 using uint = unsigned int;
 using namespace sf;
 
+void bubbleSort(RenderWindow &window, std::vector<RectangleShape> &list);
+void selectionSort(RenderWindow& window, std::vector<RectangleShape>& list);
+
 int main()
 {
-    RenderWindow window(VideoMode(700, 500), "Bubble Sort");
     const Color background(0,0,0);
     const Color item(255, 255, 255);
 
@@ -28,8 +30,33 @@ int main()
         list.at(i).setPosition(float(i), 500 - random);
     }
 
-    //main loop
-    //if(input is bubbleSort)
+    char input;
+    std::cout << "Which sorting algorithm do you want to use? (b - bubble sort, s - selection sort)" << std::endl;
+    std::cin >> input;
+    std::cin.ignore();
+
+    RenderWindow window(VideoMode(700, 500), "Sorting Algorithms");
+
+    switch (input) {
+        case 'b':
+            bubbleSort(window, list);
+            break;
+
+        case 's':
+            selectionSort(window, list);
+            break;
+
+        default:
+            std::cout << "Input not valid." << std::endl;
+            std::cin.get();
+            break;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void bubbleSort(RenderWindow &window, std::vector<RectangleShape> &list) {
+
     while (window.isOpen()) {
         static uint waitTime = 2;
         Sleep(waitTime);
@@ -41,12 +68,12 @@ int main()
         while (window.pollEvent(ev)) {
 
             switch (ev.type) {
-                case ev.Closed:
-                    window.close();
+            case ev.Closed:
+                window.close();
 
-                case ev.KeyPressed:
-                    if (ev.key.code == Keyboard::Escape)
-                        window.close();
+            case ev.KeyPressed:
+                if (ev.key.code == Keyboard::Escape)
+                    window.close();
             }
         }
 
@@ -55,7 +82,7 @@ int main()
         for (uint i = 0; i < list.size(); i++) {
             window.draw(list.at(i));
         }
-      
+
         if (i < list.size() - 1) {
             for (uint j = 0; j < list.size() - i - 1; j++) {
                 if (list[j].getSize().y > list[j + 1].getSize().y) {
@@ -63,10 +90,10 @@ int main()
                     float temp1 = list.at(j).getSize().y;
 
                     list.at(j).setPosition(list.at(j).getPosition().x, list.at(j + 1).getPosition().y);
-                    list.at(j).setSize(Vector2f(1,list.at(j+1).getSize().y));
+                    list.at(j).setSize(Vector2f(1, list.at(j + 1).getSize().y));
 
                     list.at(j + 1).setPosition(list.at(j + 1).getPosition().x, temp);
-                    list.at(j+1).setSize(Vector2f(1.0, temp1));
+                    list.at(j + 1).setSize(Vector2f(1.0, temp1));
                 }
             }
             i++;
@@ -80,10 +107,67 @@ int main()
         }
 
         window.display();
-        
+
     }
 
     window.close();
+}
 
-    return EXIT_SUCCESS;
+void selectionSort(RenderWindow& window, std::vector<RectangleShape>& list) {
+    while (window.isOpen()) {
+        static uint waitTime = 2;
+        Sleep(waitTime);
+        static uint i = 0;
+        static uint a = 0;
+
+        Event ev;
+
+        while (window.pollEvent(ev)) {
+
+            switch (ev.type) {
+            case ev.Closed:
+                window.close();
+
+            case ev.KeyPressed:
+                if (ev.key.code == Keyboard::Escape)
+                    window.close();
+            }
+        }
+
+        window.clear();
+
+        for (uint i = 0; i < list.size(); i++) {
+            window.draw(list.at(i));
+        }
+
+        if (i < list.size()) {
+            int min = i;
+            for (uint j = i; j < list.size(); j++) {
+                if (list[min].getSize().y > list[j].getSize().y) {
+                    min = j;
+                }
+                float temp = list.at(i).getPosition().y;
+                float temp1 = list.at(i).getSize().y;
+
+                list.at(i).setPosition(list.at(i).getPosition().x, list.at(min).getPosition().y);
+                list.at(i).setSize(Vector2f(1, list.at(min).getSize().y));
+
+                list.at(min).setPosition(list.at(min).getPosition().x, temp);
+                list.at(min).setSize(Vector2f(1.0, temp1));
+            }
+            i++;
+        }
+        else {
+            waitTime = .075;
+            if (a < list.size()) {
+                list.at(a).setFillColor(Color(0, 255, 0));
+                a++;
+            }
+        }
+
+        window.display();
+
+    }
+
+    window.close();
 }
